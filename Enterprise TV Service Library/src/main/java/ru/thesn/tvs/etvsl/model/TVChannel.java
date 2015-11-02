060cc03eb6d4c2e6b358bb1054fc9a6ae9f8fef1
Никита <thesn@yandex.ru>
2015-11-02 12:23:39 +0300

    --

--- Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/TVChannel.java
+++ Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/TVChannel.java
@@ -1,7 +1,34 @@
 package ru.thesn.tvs.etvsl.model;
 
-/**
- * Created by Никита on 01.11.2015.
- */
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CHANNEL")
 public class TVChannel {
+    @Id
+    @Column(name = "SOURCE_ID", nullable = false)
+    private Long sourceID;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public Long getSourceID() {
+        return sourceID;
+    }
+
+    public void setSourceID(Long sourceID) {
+        this.sourceID = sourceID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/VODContentCollection.java
+++ Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/VODContentCollection.java
@@ -1,7 +1,37 @@
 package ru.thesn.tvs.etvsl.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Никита on 01.11.2015.
  */
+
+@Entity
+@Table(name = "CONTENT")
 public class VODContentCollection {
+    @Id
+    @Column(name = "CONTENT_ID", nullable = false)
+    private Long contentID;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public Long getContentID() {
+        return contentID;
+    }
+
+    public void setContentID(Long contentID) {
+        this.contentID = contentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
