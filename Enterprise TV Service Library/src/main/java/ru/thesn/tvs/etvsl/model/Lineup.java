34e59bd3cd1c212f3493f65a910246562c102f49
Никита <thesn@yandex.ru>
2015-11-02 11:21:46 +0300

    --

--- Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/Lineup.java
+++ Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/Lineup.java
@@ -1,10 +1,33 @@
 package ru.thesn.tvs.etvsl.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "LINEUP")
 public class Lineup {
+    @Id
+    @Column(name = "AREA_ID", nullable = false)
+    private Long areaID;
 
+    @Column(name = "NAME")
+    private String name;
+
+    public Long getAreaID() {
+        return areaID;
+    }
+
+    public void setAreaID(Long areaID) {
+        this.areaID = areaID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/TVPackage.java
+++ Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/model/TVPackage.java
@@ -1,7 +1,37 @@
 package ru.thesn.tvs.etvsl.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Никита on 01.11.2015.
  */
+
+@Entity
+@Table(name = "TV_PACKAGE")
 public class TVPackage {
+    @Id
+    @Column(name = "OFFERING_ID", nullable = false)
+    private Long offeringID;
+
+    @Column(name = "NAME")
+    private String name;
+
+    public Long getOfferingID() {
+        return offeringID;
+    }
+
+    public void setOfferingID(Long offeringID) {
+        this.offeringID = offeringID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
