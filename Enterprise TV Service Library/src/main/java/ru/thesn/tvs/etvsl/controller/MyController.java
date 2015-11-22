abf9326b8226a6c4df4a85db747f61c509ba86cf
Никита <thesn@yandex.ru>
2015-11-23 00:14:35 +0300

    Исправлен баг с выдачей JSON данных

--- Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/controller/MyController.java
+++ Enterprise TV Service Library/src/main/java/ru/thesn/tvs/etvsl/controller/MyController.java
@@ -1,15 +1,11 @@
 package ru.thesn.tvs.etvsl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ru.thesn.tvs.etvsl.model.Response;
 import ru.thesn.tvs.etvsl.service.TVPackageService;
 
-@Controller
+@RestController
 @RequestMapping("/json")
 public class MyController {
 
