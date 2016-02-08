b29734eb0fb50fea2c2f129869bba8486ccd30ae
Никита <thesn@yandex.ru>
2016-02-08 12:49:05 +0300

    Теперь тесты хранятся в файле tests.txt

--- Content Authorization Service/src/main/java/ru/thesn/tvs/cas/test/TestClient.java
+++ Content Authorization Service/src/main/java/ru/thesn/tvs/cas/test/TestClient.java
@@ -1,60 +1,43 @@
 package ru.thesn.tvs.cas.test;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 
 public class TestClient {
 
     public static final String MESSAGE = "Для успешного прохождения тестов необходимо"
             + " запустить все три модуля и БД должна содержать данные только из папки test_data...";
 
-    public final static Map<String, String> TESTS;
 
-    static{
-        TESTS = new HashMap<>(); // key = url, value = expected result
-
-        TESTS.put("http://localhost:8080/rest/cas?login=admin&passwordHash=dg12a",
-                "{\"code\":\"OK\",\"errorMessage\":null,\"channels\":" +
-                "[{\"sourceID\":10011,\"name\":\"Animal Planet\",\"contentID\":9001}," +
-                "{\"sourceID\":10012,\"name\":\"Animal Planet HD\",\"contentID\":9001}," +
-                "{\"sourceID\":10031,\"name\":\"HBO HD\",\"contentID\":9003}," +
-                "{\"sourceID\":10041,\"name\":\"HBO East HD\",\"contentID\":9004}]}");
-
-        TESTS.put("http://localhost:8080/rest/cas?login=ololosha&passwordHash=Gdg4f",
-                "{\"code\":\"OK\",\"errorMessage\":null,\"channels\":[" +
-                "{\"sourceID\":10031,\"name\":\"HBO HD\",\"contentID\":9003}," +
-                "{\"sourceID\":10041,\"name\":\"HBO East HD\",\"contentID\":9004}]}");
-
-        TESTS.put("http://localhost:8080/rest/cas?login=ghost&passwordHash=dfgdfg",
-                "{\"code\":\"WARN\",\"errorMessage\":\"Уведомление: Заданный аккаунт не существует!\"," +
-                "\"channels\":null}");
-
-        TESTS.put("http://localhost:8080/rest/cas?login=admin&passwordHash=wrong_hash",
-                "{\"code\":\"ERR\",\"errorMessage\":\"Ошибка доступа: Пароль неверен!\",\"channels\":null}");
-
-        TESTS.put("http://localhost:8080/rest/cas?login=volk2005&passwordHash=gc54d",
-                "{\"code\":\"OK\",\"errorMessage\":null,\"channels\":[" +
-                "{\"sourceID\":10011,\"name\":\"Animal Planet\",\"contentID\":9001}]}");
-
-    }
+    public final static String FILE_PATH = System.getProperty("user.dir") + "\\Content Authorization Service\\src\\main\\java\\ru\\thesn\\tvs\\cas\\test\\tests.txt";
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         System.out.println(MESSAGE);
-        int i = 1;
+
+        Path path = Paths.get(FILE_PATH);
+        System.out.println(FILE_PATH);
+        if (Files.notExists(path)) throw new FileNotFoundException("Файл не существует!");
+        List<String> list = Files.readAllLines(path);
+        if (list.size() % 2 != 0) throw new IOException("Файл поврежден и содержит нечетное число строк!");
+
         boolean result = true;
-        for(Map.Entry<String, String> entry: TESTS.entrySet()){
-            boolean currentTest = entry.getValue().equals(connectAndReadJSONData(entry.getKey()));
+        for(int i = 0; i < list.size(); i+=2){
+            boolean currentTest = list.get(i+1).equals(connectAndReadJSONData(list.get(i)));
             result &= currentTest;
-            System.out.println("Test " + i + ": " + currentTest);
-            i++;
+            System.out.println("Test " + (i/2+1) + ": " + currentTest);
         }
         System.out.println("\n === " + (result ? "ПРОЙДЕНЫ ВСЕ ТЕСТЫ!" : "ЕСТЬ ОШИБКИ!") + " ===");
+
     }
 
 
